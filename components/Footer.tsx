c03715f10a369561d0acc36068224f72293dddd8
nejc000l <nejc.lampic@gmail.com>
2023-07-20 09:56:11 +0200

    most of front end done basic

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,34 +1,22 @@
 import { NavLinks } from "@/constants";
 import Link from "next/link";
 import React from "react";
-type ColumnProps = {
-    title:string;
-    links: Array<string>
-}
-const FooterColumn= ({title,links}:ColumnProps)=>(
-    <div className="footer_column">
-        <h4 className="font-semibold">
-            {title}
 
-        </h4>
-        <ul className="flex flex-col gap-2 font-normal">
-        {NavLinks.map((link) => (
-          <Link href={link.href} key={link.key}>
-            <li>{link.text}</li>
-          </Link>
-        ))}
-        </ul>
-    </div>
-)
+
 function Footer() {
   return (
-    <footer className="flexStart footer">
-      <div className="flex-flex-col gap-12 w-full">
+    <footer className=" flex flex-start footer  relative  ">
+      <div className="">
         <div className="flex items-start flex-col">
           Vidergarjeva ulica 13 Ljubljana 1261 LJUBLJANA-DOBRUNJE
         </div>
-        <div className="flex flex-wrap gap-12">
-            <FooterColumn  />
+        <div className="flex flex-wrap gap-16 ">
+        {NavLinks.map((link) => (
+            
+          <Link href={link.href} key={link.key}>
+          <li className="list-none	">{link.text}</li>
+        </Link>
+      ))}
         </div>
       </div>
     </footer>
