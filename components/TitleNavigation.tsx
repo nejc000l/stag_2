7b39a9b84d2ae44cc9086318a30db2b4254b0a57
nejc000l <nejc.lampic@gmail.com>
2023-07-28 05:00:38 +0200

    added the navigation button for admin and animated the titleNav

--- components/TitleNavigation.tsx
+++ components/TitleNavigation.tsx
@@ -4,18 +4,18 @@ import LinksMainPage from "./LinksMainPage";
 
 function TitleNavigation() {
   return (
-    <div className="h-screen paddings relative flex items-center justify-center ">
-      <div className="flex flex-col items-center justify-center p-24 flex-wrap ">
-        <div className="  font-semibold text-center absolute md:top-24 top-[16%]">
-          <h6 className="md:text-4xl text-sm  text-[#173b19] font-bold">
+    <div className="h-auto paddings absolute top-[20%]  left-0 right-0 ml-auto mr-auto flex items-center justify-center ">
+      <div className="flex flex-col items-center justify-center flex-wrap ">
+        <div className="  font-semibold text-center md:top-24 top-[16%]">
+          <h6 className="md:text-4xl text-sm text-[#173b19] font-bold">
             LOVSKA DRUŽINA
           </h6>
-          <h1 className="text-[4rem] md:text-[8rem]  drop-shadow-lg 	 text-[#aa730e]">
-            Pugled{" "}
+          <h1 className="text-[4rem] md:text-[8rem]  drop-shadow-lg text-[#aa730e]">
+            Pugled
           </h1>
         </div>
-       <KontaktSpan/>
-       <LinksMainPage/>
+        <KontaktSpan />
+        <LinksMainPage />
       </div>
     </div>
   );
