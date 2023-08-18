de01a1c8bc11d521b098a415bb06467bbd3cf161
nejc000l <nejc.lampic@gmail.com>
2023-08-18 21:13:25 +0200

    fixed some bugs

--- app/account/avatar.tsx
+++ app/account/avatar.tsx
@@ -1,114 +1,114 @@
-// "use client";
-// import React, { useEffect, useState } from "react";
-// import { Database } from "@/types/supabase";
-// import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-// type pages = Database["public"]["Tables"]["pages"]["Row"];
+"use client";
+import React, { useEffect, useState } from "react";
+import { Database } from "@/types/supabase";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+type pages = Database["public"]["Tables"]["pages"]["Row"];
 
-// export default function Avatar({
-//   uid,
-//   url,
-//   size,
-//   onUpload,
-// }: {
-//   uid: string | undefined;
-//   url: pages["avatar_url"];
-//   size: number;
-//   onUpload: (url: string) => void;
-// }) {
-//   const supabase = createClientComponentClient<Database>();
-//   const [avatarUrl, setAvatarUrl] = useState<pages["avatar_url"]>(url);
-//   const [uploading, setUploading] = useState(false);
+export default function Avatar({
+  uid,
+  url,
+  size,
+  onUpload,
+}: {
+  uid: string | undefined;
+  url: pages["avatar"];
+  size: number;
+  onUpload: (url: string) => void;
+}) {
+  const supabase = createClientComponentClient<Database>();
+  const [avatarUrl, setAvatarUrl] = useState<pages["avatar"]>(url);
+  const [uploading, setUploading] = useState(false);
 
-//   useEffect(() => {
-//     async function downloadImage(path: string) {
-//       try {
-//         const { data, error } = await supabase.storage
-//           .from("avatars")
-//           .download(path);
-//         if (error) {
-//           throw error;
-//         }
+  useEffect(() => {
+    async function downloadImage(path: string) {
+      try {
+        const { data, error } = await supabase.storage
+          .from("avatars")
+          .download(path);
+        if (error) {
+          throw error;
+        }
 
-//         const url = URL.createObjectURL(data);
-//         setAvatarUrl(url);
-//       } catch (error) {
-//         console.log("Error downloading image: ", error);
-//       }
-//     }
+        const url = URL.createObjectURL(data);
+        setAvatarUrl(url);
+      } catch (error) {
+        console.log("Error downloading image: ", error);
+      }
+    }
 
-//     if (url) downloadImage(url);
-//   }, [url, supabase]);
+    if (url) downloadImage(url);
+  }, [url, supabase]);
 
-//   const uploadAvatar: React.ChangeEventHandler<HTMLInputElement> = async (
-//     event
-//   ) => {
-//     try {
-//       setUploading(true);
+  const uploadAvatar: React.ChangeEventHandler<HTMLInputElement> = async (
+    event
+  ) => {
+    try {
+      setUploading(true);
 
-//       if (!event.target.files || event.target.files.length === 0) {
-//         throw new Error("You must select an image to upload.");
-//       }
+      if (!event.target.files || event.target.files.length === 0) {
+        throw new Error("You must select an image to upload.");
+      }
 
-//       const file = event.target.files[0];
-//       const fileExt = file.name.split(".").pop();
-//       const filePath = `${uid}-${Math.random()}.${fileExt}`;
+      const file = event.target.files[0];
+      const fileExt = file.name.split(".").pop();
+      const filePath = `${uid}-${Math.random()}.${fileExt}`;
 
-//       let { error: uploadError } = await supabase.storage
-//         .from("avatars")
-//         .upload(filePath, file);
+      let { error: uploadError } = await supabase.storage
+        .from("avatars")
+        .upload(filePath, file);
 
-//       if (uploadError) {
-//         throw uploadError;
-//       }
+      if (uploadError) {
+        throw uploadError;
+      }
 
-//       onUpload(filePath);
-//     } catch (error) {
-//       alert("Error uploading avatar!");
-//     } finally {
-//       setUploading(false);
-//     }
-//   };
+      onUpload(filePath);
+    } catch (error) {
+      alert("Error uploading avatar!");
+    } finally {
+      setUploading(false);
+    }
+  };
 
-//   return (
-//     <div>
-//       {avatarUrl ? (
-//         <iframe
-//           width={size}
-//           height={size}
-//           src={avatarUrl}
-//           className="avatar image"
-//           style={{ height: size, width: size }}
-//         />
-//       ) : (
-//         <div
-//           className="avatar no-image z-4"
-//           style={{ height: size, width: size }}
-//         />
-//       )}
-//       <div
-//         style={{
-//           width: size,
-//           zIndex: "4",
-//           display: "flex",
-//           justifyContent: "center",
-//           cursor: "pointer",
-//         }}
-//       >
-//         <label className="button primary block cursor-pointer" htmlFor="single">
-//           {uploading ? "Uploading ..." : "Upload"}
-//         </label>
-//         <input
-//           style={{
-//             visibility: "hidden",
-//             position: "absolute",
-//           }}
-//           type="file"
-//           id="single"
-//           accept="image/*"
-//           onChange={uploadAvatar}
-//           disabled={uploading}
-//         />
-//       </div>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      {avatarUrl ? (
+        <iframe
+          width={size}
+          height={size}
+          src={avatarUrl}
+          className="avatar image"
+          style={{ height: 450, width: 450 }}
+        />
+      ) : (
+        <div
+          className="avatar no-image z-4"
+          style={{ height: size, width: size }}
+        />
+      )}
+      <div
+        style={{
+          width: size,
+          zIndex: "4",
+          display: "flex",
+          justifyContent: "center",
+          cursor: "pointer",
+        }}
+      >
+        <label className="button primary block cursor-pointer" htmlFor="single">
+          {uploading ? "Uploading ..." : "Upload"}
+        </label>
+        <input
+          style={{
+            visibility: "hidden",
+            position: "absolute",
+          }}
+          type="file"
+          id="single"
+          accept="image/*"
+          onChange={uploadAvatar}
+          disabled={uploading}
+        />
+      </div>
+    </div>
+  );
+}
--- types/supabase.ts
+++ types/supabase.ts
@@ -4,85 +4,85 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export interface Database {
   public: {
     Tables: {
       pages: {
         Row: {
-          avatar_url: string | null;
-          href: string | null;
-          id: string;
-          profile_id: string;
-          text: string | null;
-          title: string | null;
-        };
+          avatar: string | null
+          href: string | null
+          id: string
+          profile_id: string
+          text: string | null
+          title: string | null
+        }
         Insert: {
-          avatar_url?: string | null;
-          href?: string | null;
-          id?: string;
-          profile_id: string;
-          text?: string | null;
-          title?: string | null;
-        };
+          avatar?: string | null
+          href?: string | null
+          id?: string
+          profile_id: string
+          text?: string | null
+          title?: string | null
+        }
         Update: {
-          avatar_url?: string | null;
-          href?: string | null;
-          id?: string;
-          profile_id?: string;
-          text?: string | null;
-          title?: string | null;
-        };
+          avatar?: string | null
+          href?: string | null
+          id?: string
+          profile_id?: string
+          text?: string | null
+          title?: string | null
+        }
         Relationships: [
           {
-            foreignKeyName: "pages_profile_id_fkey";
-            columns: ["profile_id"];
-            referencedRelation: "profiles";
-            referencedColumns: ["id"];
+            foreignKeyName: "pages_profile_id_fkey"
+            columns: ["profile_id"]
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
           }
-        ];
-      };
+        ]
+      }
       profiles: {
         Row: {
-          full_name: string | null;
-          id: string;
-          updated_at: string | null;
-          username: string | null;
-        };
+          full_name: string | null
+          id: string
+          updated_at: string | null
+          username: string | null
+        }
         Insert: {
-          full_name?: string | null;
-          id: string;
-          updated_at?: string | null;
-          username?: string | null;
-        };
+          full_name?: string | null
+          id: string
+          updated_at?: string | null
+          username?: string | null
+        }
         Update: {
-          full_name?: string | null;
-          id?: string;
-          updated_at?: string | null;
-          username?: string | null;
-        };
+          full_name?: string | null
+          id?: string
+          updated_at?: string | null
+          username?: string | null
+        }
         Relationships: [
           {
-            foreignKeyName: "profiles_id_fkey";
-            columns: ["id"];
-            referencedRelation: "users";
-            referencedColumns: ["id"];
+            foreignKeyName: "profiles_id_fkey"
+            columns: ["id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
           }
-        ];
-      };
-    };
+        ]
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
